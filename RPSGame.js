adc953b97e82c36b35770472d9edb1672a52d73e
Amy Weinberg <amyweinberg@gmail.com>
2016-07-21 15:14:02 -0400

    annoying'

--- RPSGame.js
+++ RPSGame.js
@@ -3,10 +3,11 @@ var player1 = "Amy";
 var player2 = "Latori";
 var player1Wins = 0;
 var player2Wins = 0;
+var NUMBER_WINS = 3
 
 var weapons = ['rock' , 'paper' , 'scissors'];
 
-while (player1Wins < 3 && player2Wins < 3) {
+//while (player1Wins < NUMBER_WINS && player2Wins < NUMBER_WINS) {
 
   var player1Hand = weapons[parseInt(Math.random()*weapons.length) %3];
   var player2Hand = weapons[parseInt(Math.random()*weapons.length) %3];
@@ -15,48 +16,92 @@ while (player1Wins < 3 && player2Wins < 3) {
   console.log(player2 + "'s hand this round is " + player2Hand);
 
 
-  if(player1Hand === 'rock' && player2Hand === 'paper') {
+      if(player1Hand === 'rock') {
 
-      player2Wins = player2Wins + 1;
-      console.log(player2 + " wins!");
+        if(player2Hand === 'paper') {
 
-  } else if(player1Hand === 'rock' && player2Hand === 'scissors') {
+          player2Wins = player2Wins + 1;
+          console.log(player2 + " wins!");
 
-      player1Wins = player1Wins + 1;
-      console.log(player1 + " wins!");
+        } else if (player2Hand === 'scissors') {
 
-   } else if(player1Hand === 'paper' && player2Hand === 'rock') {
+            player1Wins = player1Wins + 1;
+            console.log(player1 + " wins!");
 
-      player1Wins = player1Wins + 1;
-      console.log(player1 + " wins!");
+        } else if(player1Hand === 'paper') {
 
-   } else if (player1Hand === 'paper' && player2Hand === 'scissors') {
+          if(player2Hand === 'rock') {
 
-      player2Wins = player2Wins + 1;
-      console.log(player2 + " wins!");
+            player1Wins = player1Wins + 1;
+            console.log(player1 + " wins!");
 
-   } else if(player1Hand === 'scissors' && player2Hand === 'rock') {
+        } else if(player2 === 'scissors') {
 
-      player2Wins = player2Wins + 1;
-      console.log(player2 + " wins!");
+          player2Wins = player2Wins + 1;
+          console.log(player2 + " wins!");
+        }
 
-   } else if(player1Hand === 'scissors' && player2Hand === 'paper')
-   {
+      }
+    }
+//
+//         if(player1Hand === 'scissors') {
+//
+//           if(player2Hand === 'rock') {
+//
+//             player2Wins = player2Wins + 1;
+//             console.log(player2 + " wins!");
+//
+//           } else if (player2Hand === 'paper') {
+//
+//             player1Wins = player1Wins + 1
+//             console.log(player1 + " wins!");
+//
+//           }
+//         } else console.log("This is a tie!");
+// }
 
-      player1Wins = player1Wins + 1
-      console.log(player1 + " wins!");
-
-   } else {
-
-     console.log("This is a tie!");
-   }
-}
-
-if(player1Wins === 3) {
-
-  console.log(player1 + " is the winner!");
-
-} else {
-
-  console.log(player2 + " is the winner!");
-}
+//   if(player1Hand === 'rock' && player2Hand === 'paper') {
+//
+//       player2Wins = player2Wins + 1;
+//       console.log(player2 + " wins!");
+//
+//   } else if(player1Hand === 'rock' && player2Hand === 'scissors') {
+//
+//       player1Wins = player1Wins + 1;
+//       console.log(player1 + " wins!");
+//
+//    } else if(player1Hand === 'paper' && player2Hand === 'rock') {
+//
+//       player1Wins = player1Wins + 1;
+//     console.log(player1 + " wins!");
+//
+//    } else if (player1Hand === 'paper' && player2Hand === 'scissors') {
+//
+//       player2Wins = player2Wins + 1;
+//       console.log(player2 + " wins!");
+//
+//    } else if(player1Hand === 'scissors' && player2Hand === 'rock') {
+//
+//       player2Wins = player2Wins + 1;
+//       console.log(player2 + " wins!");
+//
+//    } else if(player1Hand === 'scissors' && player2Hand === 'paper')
+//    {
+//
+//       player1Wins = player1Wins + 1
+//       console.log(player1 + " wins!");
+//
+//    } else {
+//
+//      console.log("This is a tie!");
+//    }
+// }
+//
+// if(player1Wins === NUMBER_WINS) {
+//
+//   console.log(player1 + " is the winner!");
+//
+// } else {
+//
+//   console.log(player2 + " is the winner!");
+// }
